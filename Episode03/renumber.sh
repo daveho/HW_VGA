a855d7915d98a3e7484389031e11bd146515d1af
David Hovemeyer <david.hovemeyer@gmail.com>
2024-07-11 09:42:47 -0400

    renumbered .xopp files
    It's looking like there will be more than 100 graphic
    frames for the readout module overview.

--- Episode03/renumber.sh
+++ Episode03/renumber.sh
@@ -2,5 +2,8 @@
 
 for f in readout-overview-??.xopp; do
   new_number=$(echo $f | perl -ne '/(\d\d)/ && printf "%03d\n", $1')
-  echo git mv $f readout-overview-${new_number}.xopp
+  echo "Renaming $f to readout-overview-${new_number}.xopp..."
+  git mv $f readout-overview-${new_number}.xopp
 done
+
+echo "done"
